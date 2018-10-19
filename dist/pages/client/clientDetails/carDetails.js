a5840fb58d59629a5f27fb61ecff4876f691ba23
TangWanZun <997017913@qq.com>
2018-10-19 18:08:52 +0800

    2018.10.19

---
+++ dist/pages/client/clientDetails/carDetails.js
@@ -0,0 +1,68 @@
+// pages/client/clientDetails/carDetails.js.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    formData:{
+      carImg: 'url(http://img4.cache.netease.com/photo/0008/2016-04-29/BLQM14CK296H0008.jpg)',
+      carLogo: 'url(https://tse2.mm.bing.net/th?id=OIP.ljO24bOULoKVMyTf4NiM9gAAAA&pid=Api)'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
