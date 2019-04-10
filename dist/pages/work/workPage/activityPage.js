1987ce0cebed37912e9f1ec0a167f4aab93612a2
TangWanZun <997017913@qq.com>
2019-04-10 18:01:09 +0800

    2019年4月10日

--- dist/pages/work/workPage/activityPage.js
+++ dist/pages/work/workPage/activityPage.js
@@ -1,71 +1,74 @@
 import createPage from './workPage.js'
-import { toDateTime, GUID} from '../../../library/sdk/util.js'
+import {
+    toDateTime,
+    GUID
+} from '../../../library/sdk/util.js'
 
 import {
-        configUrl
+    configUrl
 } from '../../../library/sdk.js'
 Page(createPage({
-        data: {
-                imgUrl: configUrl.imgUrl,
-        },
-        /**
-        * 生命周期函数--监听页面加载
-        */
-        onLoad: function (_this, options) {
-                wx.showLoading({
-                        title: '数据加载中',
+    data: {
+        imgUrl: configUrl.imgUrl,
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(_this, options) {
+        wx.showLoading({
+            title: '数据加载中',
+        })
+        wx.$request({
+            url: "/WeMinProPlatJson/GetList",
+            data: {
+                docType: 'WorkData',
+                actionType: 'ActSignUpDetails',
+                docid: options.id,
+                needTotal: false,
+            },
+            success(res) {
+                let data = res[0]
+                //整理数据
+                //报名时间
+                data.CreateDate = toDateTime(data.CreateDate)
+                _this.setData({
+                    formData: data
                 })
-                wx.$request({
-                        url: "/WeMinProPlatJson/GetList",
-                        data: {
-                                docType: 'WorkData',
-                                actionType: 'ActSignUpDetails',
-                                docid: options.id,
-                                needTotal: false,
-                        },
-                        success(res) {
-                                let data = res[0]
-                                //整理数据
-                                //报名时间
-                                data.CreateDate = toDateTime(data.CreateDate)
-                                _this.setData({
-                                        formData: data
-                                })
-                        },
-                        complete() {
-                                wx.hideLoading();
-                        }
-                })
-        },
-        methods: {
-                submit() {
-                        wx.showLoading({
-                                title: '数据提交中',
-                        })
-                        let _formData = this.data.formData;
-                        let _this = this;
-                        wx.$request({
-                                url: "/WeMinProActivitySignUp/Submit",
-                                data: {
-                                        Id: _formData.Id,
-                                        DocStatus: _formData.DocStatus,
-                                        Note: _formData.Note || '',
-                                        UnionGuidTemp:GUID(),
-                                        UnionGuid: _formData.UnionGuid
-                                },
-                                success(res) {
-                                        console.log(res);
-                                        wx.showToast({
-                                                title: '提交成功',
-                                        })
-                                        _this.setData({
-                                                sendDataDisabled: true
-                                        })
-                                },
-                                complete() {
-                                        wx.hideLoading();
-                                }
-                        })
+            },
+            complete() {
+                wx.hideLoading();
+            }
+        })
+    },
+    methods: {
+        submit() {
+            wx.showLoading({
+                title: '数据提交中',
+            })
+            let _formData = this.data.formData;
+            let _this = this;
+            wx.$request({
+                url: "/WeMinProActivitySignUp/Submit",
+                data: {
+                    Id: _formData.Id,
+                    DocStatus: _formData.DocStatus,
+                    Note: _formData.Note || '',
+                    UnionGuidTemp: GUID(),
+                    UnionGuid: _formData.UnionGuid
+                },
+                success(res) {
+                    console.log(res);
+                    wx.showToast({
+                        title: '提交成功',
+                    })
+                    _this.setData({
+                        sendDataDisabled: true
+                    })
+                },
+                complete() {
+                    wx.hideLoading();
                 }
+            })
         }
+    }
 }))
