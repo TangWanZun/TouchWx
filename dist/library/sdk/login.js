05c8b65d25fd69a63c560d67fd0ea214414c4c74
TangWanZun <997017913@qq.com>
2019-04-11 18:23:25 +0800

    2019年4月11日

--- dist/library/sdk/login.js
+++ dist/library/sdk/login.js
@@ -1,89 +1,89 @@
 //保存用户信息
 function saveUserInfo(res, obj = {}) {
-        // console.log("success", res);
-        let privateData = getApp().privateData;
-        //将Token保存到内存
-        privateData.Token = res.Token;
-        //将个人信息保存到内存
-        privateData.loginInfo = res.LoginInfo;
-        //当用户连接上时，发送重新发送滞留消息
-        privateData.requestRetention.forEach(wx.$request);
-        //清空滞留消息
-        privateData.requestRetention.length = 0;
-        //保存Token信息持久化
-        wx.setStorage({
-                key: 'Token',
-                data: res.Token,
-                success(response) {
-                        //保存到内存
-                        obj.success && obj.success(response);
-                }
-        });
-        //加载权限
-        wx.$request({
-                url: "/WeMinProPlatJson/GetList",
-                data: {
-                        docType: 'Main',
-                        actionType: 'Permission',
-                        needTotal: false,
-                },
-                success(res) {
-                        let list = [];
-                        //对配置信息进行处理
-                        for(let x of res){
-                                list[x.ActionType] = x.Permission;
-                        }
-                        //将配置信息保存
-                        privateData.UXList = list
-                },
-                complete(res){
-                        //当全部信息获取之后,调用加载滞留池中的方法
-                        getApp().privateData.loadRetention.forEach(getApp().loadInfo);
-                }
-        })
+    // console.log("success", res);
+    let privateData = getApp().privateData;
+    //将Token保存到内存
+    privateData.Token = res.Token;
+    //将个人信息保存到内存
+    privateData.loginInfo = res.LoginInfo;
+    //当用户连接上时，发送重新发送滞留消息
+    privateData.requestRetention.forEach(wx.$request);
+    //清空滞留消息
+    privateData.requestRetention.length = 0;
+    //保存Token信息持久化
+    wx.setStorage({
+        key: 'Token',
+        data: res.Token,
+        success(response) {
+            //保存到内存
+            obj.success && obj.success(response);
+        }
+    });
+    //加载权限
+    wx.$request({
+        url: "/WeMinProPlatJson/GetList",
+        data: {
+            docType: 'Main',
+            actionType: 'Permission',
+            needTotal: false,
+        },
+        success(res) {
+            let list = [];
+            //对配置信息进行处理
+            for (let x of res) {
+                list[x.ActionType] = x.Permission;
+            }
+            //将配置信息保存
+            privateData.UXList = list
+        },
+        complete(res) {
+            //当全部信息获取之后,调用加载滞留池中的方法
+            getApp().privateData.loadRetention.forEach(getApp().loadInfo);
+        }
+    })
 }
 //测试信息调取
 function loginTest() {
-        // var Token =  wx.getStorageSync('Token');
-        // console.log(Token);
-        //获取code
-        wx.login({
-                success: function(loginRes) {
-                        // 将code传到后台，并换取后台自定义的用户登录态标识
-                        wx.$request({
-                                url: '/WeMinProLogin/Login',
-                                data: {
-                                        code: loginRes.code
-                                },
-                                UV: true,
-                                success(res) {
-                                        saveUserInfo(res);
-                                },
-                                fail(res) {
-                                        wx.showModal({
-                                                title: '登录以过期',
-                                                content: '点击确定重新登录',
-                                                showCancel: false,
-                                                success(resin) {
-                                                        if (resin.confirm) {
-                                                                //记录当前登录状态
-                                                                saveUserInfo(res, {
-                                                                        success: function() {
-                                                                                // 进入登陆页
-                                                                                wx.reLaunch({
-                                                                                        url: "/pages/tabBar/login"
-                                                                                })
-                                                                        }
-                                                                })
-                                                        }
-                                                }
+    // var Token =  wx.getStorageSync('Token');
+    // console.log(Token);
+    //获取code
+    wx.login({
+        success: function(loginRes) {
+            // 将code传到后台，并换取后台自定义的用户登录态标识
+            wx.$request({
+                url: '/WeMinProLogin/Login',
+                data: {
+                    code: loginRes.code
+                },
+                UV: true,
+                success(res) {
+                    saveUserInfo(res);
+                },
+                fail(res) {
+                    wx.showModal({
+                        title: '登录以过期',
+                        content: '点击确定重新登录',
+                        showCancel: false,
+                        success(resin) {
+                            if (resin.confirm) {
+                                //记录当前登录状态
+                                saveUserInfo(res, {
+                                    success: function() {
+                                        // 进入登陆页
+                                        wx.reLaunch({
+                                            url: "/pages/tabBar/login"
                                         })
-                                }
-                        })
+                                    }
+                                })
+                            }
+                        }
+                    })
                 }
-        });
+            })
+        }
+    });
 }
 export {
-        loginTest as
-        default
+    loginTest as
+    default
 }
