e3f748cbb881a24242efc7171f89e0a0e394f4bf
TangWanZun <997017913@qq.com>
2019-05-13 18:07:32 +0800

    2019年5月13日

--- dist/pages/user/myData.js
+++ dist/pages/user/myData.js
@@ -1,103 +1,174 @@
-// import * as echarts from '../../library/ec-canvas/echarts';
+import * as echarts from '../../library/ec-canvas/echarts';
 
-// let chart = null;
+let chart = null;
 
-// function initChart(canvas, width, height) {
-//   chart = echarts.init(canvas, null, {
-//     width: width,
-//     height: height
-//   });
-//   canvas.setChart(chart);
-//   var option = {
-//     title:{
-//       text:'月产值(点击图表查看详情)',
-//       textStyle:{
-//         fontSize:16
-//       }
-//     },
-//     tooltip: {
-//       trigger: 'axis',
-//       axisPointer: {
-//         type: 'cross',
-//         crossStyle: {
-//           color: '#999'
-//         },
-//       },
-//       // 绝对位置，相对于容器左侧 10px, 上侧 10 px
-//       position: [10, 10]
-//     },
-//     legend: {
-//       data: ['月产值(元)', '店内排名'],
-//       bottom:5
-//     },
-//     xAxis: [
-//       {
-//         type: 'category',
-//         data: ['7月', '8月', '9月', '10月', '11月', '12月'],
-//         axisPointer: {
-//           type: 'shadow'
-//         }
-//       }
-//     ],
-//     yAxis: [
-//       {
-//         type: 'value',
-//         name: '月产值(万)',
-//         axisLabel: {
-//           formatter: function (value, index) {
-//             return value / 10000
-//           },
-//         }
-//       },
-//       {
-//         type: 'value',
-//         name: '店内排名',
-//         max: 20,
-//         min:  1,
-//         inverse:true,
-//         interval:5,
-//         splitLine:{
-//           show:false
-//         },
-//         nameLocation:"start"
-//       }
-//     ],
-//     series: [
-//       {
-//         name: '月产值(元)',
-//         type: 'bar',
-//         data: [156200, 182100, 156200, 156200, 556200, 312200]
-//       },
-//       {
-//         name: '店内排名',
-//         type: 'line',
-//         yAxisIndex: 1,
-//         data: [6, 5, 5, 2, 1, 3]
-//       }
-//     ]
-//   };
-//   chart.setOption(option);
-//   return chart;
-// }
+// 会员数量占比
+function initChart1(canvas, width, height) {
+    chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+    });
+    canvas.setChart(chart);
+    var option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} {b}: {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            data: ['会员客户', '非会员客户'],
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        series: [{
+            name: '',
+            type: 'pie',
+            radius: '77%',
+            roseType: 'radius',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'outside',
+                    formatter: "{b}\n{d}% \n {c}",
+                },
+                // emphasis: {
+                //     show: true,
+                //     textStyle: {
+                //         fontSize: '30',
+                //         fontWeight: 'bold'
+                //     }
+                // }
+            },
+            labelLine: {
+                normal: {
+                    show: true
+                }
+            },
+            data: [{
+                    value: 21762,
+                    name: '会员客户'
+                },
+                {
+                    value: 37891,
+                    name: '非会员客户'
+                }
+            ],
+            itemStyle: {
+                normal: {
+                    color: function(params) {
+                        //自定义颜色
+                        var colorList = [
+                            '#ff5f5f', '#3b9f92', '#FFFF00', '#FF8C00', '#FF0000', '#FE8463',
+                        ];
+                        return colorList[params.dataIndex]
+                    }
+                }
+            }
+        }]
+    };
+    chart.setOption(option);
+    return chart;
+}
 
-// Page({
-//   onShareAppMessage: function (res) {
-//     return {
-//       title: 'ECharts 可以在微信小程序中使用啦！',
-//       path: '/pages/user/myData',
-//       success: function () { },
-//       fail: function () { }
-//     }
-//   },
-//   data: {
-//     ec: {
-//       onInit: initChart
-//     }
-//   },
-//   onReady() {
-//     setTimeout(function () {
-//       // 获取 chart 实例的方式
-//       console.log(chart)
-//     }, 2000);
-//   }
-// });
+// 产值数量占比
+function initChart2(canvas, width, height) {
+    chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+    });
+    canvas.setChart(chart);
+    var option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} {b}: {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            data: ['低产值', '中产值','高产值'],
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        series: [{
+            name: '',
+            type: 'pie',
+            radius: '55%',
+            roseType:'radius',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'outside',
+                    formatter: "{b}\n{d}% \n {c}",
+                },
+                // emphasis: {
+                //     show: true,
+                //     textStyle: {
+                //         fontSize: '30',
+                //         fontWeight: 'bold'
+                //     }
+                // }
+            },
+            labelLine: {
+                normal: {
+                    show: true
+                }
+            },
+            data: [{
+                value: 9461,
+                name: '低产值'
+            },
+            {
+                value: 30172,
+                name: '中产值'
+            },
+            {
+                value: 9162,
+                name: '高产值'
+            }
+            ],
+            itemStyle: {
+                normal: {
+                    color: function (params) {
+                        //自定义颜色
+                        var colorList = [
+                            '#d23aba', '#4fd89c', '#794fd8', '#FF8C00', '#FF0000', '#FE8463',
+                        ];
+                        return colorList[params.dataIndex]
+                    }
+                }
+            }
+        }]
+    };
+    chart.setOption(option);
+    return chart;
+}
+
+Page({
+    //   onShareAppMessage: function (res) {
+    //     return {
+    //       title: 'ECharts 可以在微信小程序中使用啦！',
+    //       path: '/pages/user/myData',
+    //       success: function () { },
+    //       fail: function () { }
+    //     }
+    //   },
+    data: {
+        // 会员数量占比
+        ec1: {
+            onInit: initChart1
+        },
+        // 产值数量占比
+        ec2: {
+            onInit: initChart2
+        }
+    },
+    onReady() {
+        // setTimeout(function () {
+        //   // 获取 chart 实例的方式
+        //   console.log(chart)
+        // }, 2000);
+    }
+});
