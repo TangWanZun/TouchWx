0981dd61c755417c84fa0321bad2a0b0d66aeedd
TangWanZun <997017913@qq.com>
2019-06-12 18:05:09 +0800

    2019年6月12日

--- dist/pages/user/cmpScopeFilter.js
+++ dist/pages/user/cmpScopeFilter.js
@@ -1,5 +1,8 @@
 // pages/user/cmpScopeFilter.js
-import { CMP_CAR} from "../../library/sdk/config.js"
+import {
+    CMP_CAR,
+    CMP_REGION
+} from "../../library/sdk/config.js"
 Page({
 
     /**
@@ -8,14 +11,159 @@ Page({
     data: {
         //获取的数据
         formList: [],
-        CMP_CAR: CMP_CAR
+        CMP_CAR: CMP_CAR,
+        CMP_REGION: CMP_REGION,
+        //筛选的属性
+        selectItem: {
+            //汽车类型
+            BrandType: "",
+            //公司区域
+            RegionType: ""
+        },
+        //侧边栏是否出现
+        sideEdgeShow: false,
     },
 
     /*
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        this.getData(true)
+        this.getData(true);
+        //首先需要对获取的类型数据进行处理
+        let CMP_CAR_OBJ = {};
+        for (let x in CMP_CAR) {
+            CMP_CAR_OBJ[x] = {
+                name: CMP_CAR[x],
+                code: x,
+                _isSelect: true
+            }
+        }
+        CMP_CAR_OBJ['A00'] = {
+            name: '未定义',
+            code: 'A00',
+            _isSelect: true
+        }
+        let CMP_REGION_OBJ = {}
+        for (let x in CMP_REGION) {
+            CMP_REGION_OBJ[x] = {
+                name: CMP_REGION[x],
+                code: x,
+                _isSelect: true
+            }
+        }
+        CMP_REGION_OBJ['A00'] = {
+            name: '未定义',
+            code: 'A00',
+            _isSelect: true
+        }
+        this.setData({
+            'selectItem.BrandType': CMP_CAR_OBJ,
+            'selectItem.RegionType': CMP_REGION_OBJ,
+        })
+    },
+    /**
+     * 多选单项改变
+     */
+    changeCheck(e) {
+        let index = e.currentTarget.dataset.index;
+        let isauth = e.currentTarget.dataset.isauth;
+        this.setData({
+            [`formList[${index}].IsAuth`]: !isauth
+        })
+    },
+    /**
+     * 添加筛选
+     */
+    selectClick(e) {
+        //筛选类型
+        let setType = e.currentTarget.dataset.type;
+        //筛选项的key值
+        let index = e.currentTarget.dataset.index;
+        // console.log(this.data.selectItem);
+        this.setData({
+            [`selectItem.${setType}.${index}._isSelect`]: !this.data.selectItem[setType][index]._isSelect
+        })
+        //这里需要重新对页面公司进行排序
+        this.hintItem();
+    },
+    /**
+     * 根据筛选类型对数据进行排序
+     */
+    hintItem() {
+        let selectItem = this.data.selectItem;
+        let cmpList = this.data.formList;
+        //需要排除的选项
+        let selectOutObj = {};
+        //首先获取全部的的筛选项
+        for (let x in selectItem) {
+            selectOutObj[x] = {}
+            for (let index in selectItem[x]) {
+                let item = selectItem[x][index];
+                if (!item._isSelect) {
+                    //表示当前筛选项是false类型的 存在这个类型的是需要排除出去的
+                    selectOutObj[x][index] = item;
+                }
+            }
+        }
+        //设置需要检验的属性
+        let attr = ['BrandType', 'RegionType'];
+        //遍历全部的公司信息
+        for (let cmp of cmpList) {
+            //首先将这个公司默认为展示
+            cmp._isShow = true;
+            //将全部公司默认为选中
+            cmp.IsAuth = true;
+            for (let attrItem of attr) {
+                //判断当前类型是否在排除选项中
+                //我们这里用A00表示未选中的
+                if (selectOutObj[attrItem][cmp[attrItem] || 'A00']) {
+                    //这个属性 的值在排除项中  所以说这公司是需要排除的
+                    cmp._isShow = false;
+                    cmp.IsAuth = false;
+                    //退出循环
+                    break
+                }
+            }
+        }
+        //更新到视图上
+        this.setData({
+            formList: cmpList
+        })
+        // console.log(cmpList)
+    },
+    /**
+     * 打开侧边栏
+     */
+    showSideEdge() {
+        this.setData({
+            sideEdgeShow: true
+        })
+    },
+    /**
+     * 关闭侧边栏
+     */
+    closeSideEdge() {
+        this.setData({
+            sideEdgeShow: false
+        })
+    },
+    /**
+     * 侧边栏重置
+     */
+    sideEdgeReset() {
+        let selectItem = this.data.selectItem;
+        //首先获取全部的的筛选项
+        for (let x in selectItem) {
+            for (let index in selectItem[x]) {
+                //全部选择
+                selectItem[x][index]._isSelect = true;
+            }
+        }
+        //重新筛选排序
+        this.hintItem();
+        this.setData({
+            selectItem: selectItem
+        })
     },
     //这个是用于提交的数据，是不需要放置到data中的
     // submitList:[],
@@ -40,58 +188,16 @@ Page({
                 needTotal: false
             },
             success: (res) => {
-
-                //将上传数据的默认的不选中
-                //因为  微信小程序中 只会返回选中的值
-                // for(let item of res){
-                //     let pushItem =Object.assign({},item,{
-                //         IsAuth: false
-                //     })
-                //     this.submitList.push(pushItem)
-                // }
-
-                let dataList = {};
-                for (let i in res){
-                    let item = res[i];
-                    //不存在父节点的为根节点
-                    if (!item.GroupCode){
-                        dataList[item.CmpCode] = Object.assign(dataList[item.CmpCode]||{},{
-                            CmpCode: item.CmpCode,
-                            CmpName: item.CmpName,
-                            IsAuth: item.IsAuth,
-                            list: {}
-                        })
-                        continue
-                    }
-                    //表示存在父节点的公司
-                    if (!dataList[item.GroupCode]){
-                        //没有当前公司的父公司的时候创建一个父公司
-                        dataList[item.GroupCode] = {
-                            CmpCode: item.GroupCode,
-                            list:{}
-                        };
-                    }
-                    let childList = dataList[item.GroupCode].list;
-                    //对系列进行分类
-                    if (!childList[item.BrandType]){
-                        //当不存在这个系列的时候
-                        childList[item.BrandType] = {
-                            //默认为选中状态
-                            IsAuth:1,
-                            //是否已经收起来了
-                            _isShow:true,
-                            list:[]
-                        };
-                    }
-                    if (!item.IsAuth){
-                        //当有一个子项为不存在的时候
-                        childList[item.BrandType].IsAuth = 0;
-                    }
-                    childList[item.BrandType].list.push(item);
+                let submitList = [];
+                for (let item of res) {
+                    let pushItem = Object.assign(item, {
+                        //是否展示
+                        _isShow: true
+                    })
+                    submitList.push(pushItem)
                 }
-                // console.log(dataList)
                 this.setData({
-                    formList: dataList
+                    formList: submitList,
                 })
             },
             complete() {
@@ -104,8 +210,9 @@ Page({
             }
         })
     },
+
     /**
-     * 重置
+     * 外部重置
      */
     reset() {
         wx.showModal({
@@ -124,6 +231,8 @@ Page({
                             console.log(res)
                             //重新获取数据
                             this.getData(true);
+                            //重置筛选框
+                            this.sideEdgeReset();
                         },
                         complete() {
                             wx.stopPullDownRefresh();
@@ -134,9 +243,39 @@ Page({
         })
     },
     /**
+     * 保存
+     */
+    submit() {
+        // console.log(this.data.formList);
+        let formList = this.data.formList;
+        // console.log(list)
+        //需要对上传数据进行处理
+        wx.$request({
+            url: '/WeMinProPlatJson/Submit',
+            data: {
+                docType: 'Main',
+                actionType: 'SubmitCsf',
+                docJson: JSON.stringify({
+                    'List': formList
+                })
+            },
+            success: (res) => {
+                //重新获取数据
+                this.getData(true);
+                //重置筛选框
+                this.sideEdgeReset();
+            },
+            complete() {
+                wx.stopPullDownRefresh();
+            }
+        })
+    },
+    // ============================================================
+
+    /**
      * 点击父公司的钩子
      */
-    changeRootCheck(e){
+    changeRootCheck(e) {
         //公司根
         let root = e.currentTarget.dataset.root;
         //当前类型
@@ -148,8 +287,8 @@ Page({
         //将子模块全选
         for (let i in checkList) {
             this.changeCarTypeCheck({
-                currentTarget:{
-                    dataset:{
+                currentTarget: {
+                    dataset: {
                         root: root,
                         cartype: i
                     }
@@ -157,12 +296,12 @@ Page({
             })
             checkList[i].IsAuth = 1;
         }
-        
+
     },
     /**
      * 点击汽车类型的钩子
      */
-    changeCarTypeCheck(e){
+    changeCarTypeCheck(e) {
         //公司根
         let root = e.currentTarget.dataset.root;
         //汽车类型
@@ -172,7 +311,7 @@ Page({
         //改变当前的钩子的点击
         let check = carTypeObj.IsAuth = !(carTypeObj.IsAuth);
         let checkList = carTypeObj.list;
-        for (let i in checkList){
+        for (let i in checkList) {
             checkList[i].IsAuth = check;
         }
         // 更新数据
@@ -183,7 +322,7 @@ Page({
     /**
      * 点击汽车类型右侧的打开
      */
-    tapRightImage(e){
+    tapRightImage(e) {
         //公司根
         let root = e.currentTarget.dataset.root;
         //汽车类型
@@ -199,7 +338,7 @@ Page({
     /**
      * 用来监控每个车类型下下面的全部打勾的
      */
-    cartypeBindChange(e){
+    cartypeBindChange(e) {
         //获取全部打勾的公司
         let valueList = e.detail.value;
         //公司根
@@ -208,13 +347,13 @@ Page({
         let cartype = e.currentTarget.dataset.cartype;
         //获取当前汽车类型下的全部数据
         let list = this.data.formList[root].list[cartype].list;
-        if (valueList.length == list.length){
+        if (valueList.length == list.length) {
             //表示该汽车类型下的全部公司都已经打勾了
             //则该类型也是需要打勾的
             this.setData({
-                [`formList.${root}.list.${cartype}.IsAuth`]:1
+                [`formList.${root}.list.${cartype}.IsAuth`]: 1
             })
-        }else{
+        } else {
             this.setData({
                 [`formList.${root}.list.${cartype}.IsAuth`]: 0
             })
@@ -223,7 +362,7 @@ Page({
     /**
      * 点击cmp
      */
-    cmpBindChange(e){
+    cmpBindChange(e) {
         //公司根
         let root = e.currentTarget.dataset.root;
         //汽车类型
@@ -234,56 +373,8 @@ Page({
             [`formList.${root}.list.${cartype}.list[${index}].IsAuth`]: !this.data.formList[root].list[cartype].list[index].IsAuth
         })
     },
-    /**
-     * 保存
-     */
-    submit() {
-        // console.log(this.data.formList);
-        let formList = this.data.formList;
-        let list = []
-        //root
-        for (let i in formList){
-            list.push({
-                CmpCode: formList[i].CmpCode,
-                CmpName: formList[i].CmpName,
-                IsAuth: 1
-            })
-            //汽车类型
-            for (let j in formList[i].list){
-                //公司
-                for(let item of formList[i].list[j].list){
-                    list.push(item)
-                }
-            }
-        }
-        // console.log(list)
-        //需要对上传数据进行处理
-        wx.$request({
-            url: '/WeMinProPlatJson/Submit',
-            data: {
-                docType: 'Main',
-                actionType: 'SubmitCsf',
-                docJson: JSON.stringify({
-                    'List': list
-                })
-            },
-            success: (res) => {
-                //重新获取数据
-                this.getData(true);
-            },
-            complete() {
-                wx.stopPullDownRefresh();
-            }
-        })
-    },
-    /**
-     * 多选单项改变
-     */
-    changeCheck(e) {
-        this.setData({
-            [`formList[${e.currentcurrentTarget.dataset.index}].IsAuth`]: !e.currentcurrentTarget.dataset.isauth
-        })
-    },
+    
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
