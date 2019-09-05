954f272206a7200be10bd24c4ee57bb6bb0ae246
TangWanZun <997017913@qq.com>
2019-09-05 18:17:09 +0800

    2019年9月5日

---
+++ dist/pages/user/sweepCode.js
@@ -0,0 +1,92 @@
+// pages/user/sweepCode.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList:[
+      {
+        ImgUrl: "/assets/sweepCode/6.svg",
+        Name: "用户信息"
+      },
+      {
+        ImgUrl:"/assets/sweepCode/1.svg",
+        Name:"业务卡券核销"
+      },
+      {
+        ImgUrl: "/assets/sweepCode/2.svg",
+        Name: "预约到店登记"
+      },
+      {
+        ImgUrl: "/assets/sweepCode/3.svg",
+        Name: "餐券核销"
+      },
+      {
+        ImgUrl: "/assets/sweepCode/4.svg",
+        Name: "洗车券核销"
+      },
+      {
+        ImgUrl: "/assets/sweepCode/5.svg",
+        Name: "档案维护"
+      },
+      
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
